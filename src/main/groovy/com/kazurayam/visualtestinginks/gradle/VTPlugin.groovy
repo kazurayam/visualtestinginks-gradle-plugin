b45a300be590c8ed0de11fd23f9159ee53a75e29
kazurayam <kazuaki.urayama@gmail.com>
2019-04-18 22:30:28 +0900

    problem https://github.com/gradle/gradle/issues/1262 occured; workaround found and VTPluginSpec success; de.undercouch.download plugin is found

--- src/main/groovy/com/kazurayam/visualtestinginks/gradle/VTPlugin.groovy
+++ src/main/groovy/com/kazurayam/visualtestinginks/gradle/VTPlugin.groovy
@@ -12,8 +12,8 @@ class VTPlugin implements Plugin<Project> {
     private void applyPlugins(Project project) {
         project.getPlugins().apply(GroovyPlugin.class)
 
-	// https://github.com/michel-kraemer/gradle-download-task
-	project.getPlugins().apply('de.undercouch.download')
+        // https://github.com/michel-kraemer/gradle-download-task
+        project.getPlugins().apply('de.undercouch.download')
     }
 
     public void apply(Project project) {
@@ -29,7 +29,7 @@ class VTPlugin implements Plugin<Project> {
             }
         }
         // vt-build.gradle
-	Task cleanDist             = project.getTasks().create('cleanDist', CleanDist.class)
+        Task cleanDist             = project.getTasks().create('cleanDist', CleanDist.class)
         Task createGradlePackaged  = project.getTasks().create('createGradlePackaged', CreateGradlePackaged)
         Task createVTComponentsZip = project.getTasks().create('createVTComponentsZip', CreateVTComponentsZip)
         Task createVTExampleZip    = project.getTasks().create('createVTExampleZip', CreateVTExampleZip)
@@ -42,44 +42,44 @@ class VTPlugin implements Plugin<Project> {
         createVTExampleZip.mustRunAfter('cleanDist')
 
         // vt-drivers.gradle
-	List<String> urlList = [
-	    'http://central.maven.org/maven2/ru/yandex/qatools/ashot/ashot/1.5.4/ashot-1.5.4.jar',
-	    'http://central.maven.org/maven2/net/coobird/thumbnailator/0.4.8/thumbnailator-0.4.8.jar',
-      	    'http://central.maven.org/maven2/org/apache/commons/commons-lang3/3.6/commons-lang3-3.6.jar',
-    	    'http://central.maven.org/maven2/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar',
-	    'https://github.com/kazurayam/junit4ks/releases/download/1.6/junit4ks-all.jar',
-	    'https://github.com/kazurayam/ksbackyard/releases/download/0.36.0/ksbackyard.jar',
-	    'https://github.com/kazurayam/Materials/releases/download/0.68.0/Materials-0.68.0.jar'
-	    ]
+        List<String> urlList = [
+            'http://central.maven.org/maven2/ru/yandex/qatools/ashot/ashot/1.5.4/ashot-1.5.4.jar',
+            'http://central.maven.org/maven2/net/coobird/thumbnailator/0.4.8/thumbnailator-0.4.8.jar',
+            'http://central.maven.org/maven2/org/apache/commons/commons-lang3/3.6/commons-lang3-3.6.jar',
+            'http://central.maven.org/maven2/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar',
+            'https://github.com/kazurayam/junit4ks/releases/download/1.6/junit4ks-all.jar',
+            'https://github.com/kazurayam/ksbackyard/releases/download/0.36.0/ksbackyard.jar',
+            'https://github.com/kazurayam/Materials/releases/download/0.68.0/Materials-0.68.0.jar'
+            ]
 
-	Path outDir = project.projectDir.toPath().resolve('Drivers')
-	
-	Task deleteJarsInDriversDir     = project.getTasks().create('deleteJarsInDriversDir', DeleteJarsInDriversDir.class)
+        Path outDir = project.projectDir.toPath().resolve('Drivers')
+
+        Task deleteJarsInDriversDir     = project.getTasks().create('deleteJarsInDriversDir', DeleteJarsInDriversDir.class)
 
         Task downloadJarsIntoDriversDir = project.getTasks().create('downloadJarsIntoDriversDir') {
-            doLast {    	
-		for (String url: urlList) {
-		    // download extension is provided by the community plugin "de.undercouch.download"
+            doLast {
+                for (String url: urlList) {
+                    // download extension is provided by the community plugin "de.undercouch.download"
                     download {
                         src url
-		        dest outDir.resolve("${url.tokenize('/')[-1]}").toFile()
+                        dest outDir.resolve("${url.tokenize('/')[-1]}").toFile()
                     }
-		    project.logger.info("downloaded ${url} into ${outDir}")
-		}
+                    project.logger.info("downloaded ${url} into ${outDir}")
+                }
             }
         }
-	
-	Task drivers = project.getTasks().create('drivers')
-	drivers.dependsOn(deleteJarsInDriversDir)
-	drivers.dependsOn(downloadJarsIntoDriversDir)
-	downloadJarsIntoDriversDir.mustRunAfter('deleteJarsInDriversDir')
+
+        Task drivers = project.getTasks().create('drivers')
+        drivers.dependsOn(deleteJarsInDriversDir)
+        drivers.dependsOn(downloadJarsIntoDriversDir)
+        downloadJarsIntoDriversDir.mustRunAfter('deleteJarsInDriversDir')
 
         // vt-init.gradle
-	Task unzipVTComponents = project.getTasks().create('unzipVTComponents', UnzipVTComponents.class)
-	Task unzipVTExample    = project.getTasks().create('unzipVTExample', UnzipVTExample.class)
-	Task importVT = project.getTasks().create('importVT')
-	importVT.dependsOn(unzipVTComponents)
-	importVT.dependsOn(unzipVTExample)
+        Task unzipVTComponents = project.getTasks().create('unzipVTComponents', UnzipVTComponents.class)
+        Task unzipVTExample    = project.getTasks().create('unzipVTExample', UnzipVTExample.class)
+        Task importVT = project.getTasks().create('importVT')
+        importVT.dependsOn(unzipVTComponents)
+        importVT.dependsOn(unzipVTExample)
     }
 
 }
